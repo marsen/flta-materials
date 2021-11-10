a4d5ed96de3a7e234453824fccc70c4fbe1810cb
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-10 10:58:18 +0800

    make card1 display

--- 03-basic-widgets/projects/starter/lib/card1.dart
+++ 03-basic-widgets/projects/starter/lib/card1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'fooderlich_theme.dart';
+
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
 
@@ -14,8 +16,38 @@ class Card1 extends StatelessWidget {
   Widget build(BuildContext context) {
     // 3
     return Center(
-      // TODO: Card1 Decorate Container
       child: Container(
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline5,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            ),
+          ],
+        ),
         padding: const EdgeInsets.all(16),
         constraints: const BoxConstraints.expand(
           width: 350,
--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -13,7 +13,7 @@ class Fooderlich extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.light();
+    final theme = FooderlichTheme.dark();
     // 3
     return MaterialApp(
       theme: theme,
