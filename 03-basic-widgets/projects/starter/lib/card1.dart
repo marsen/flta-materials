8652aac1eacd69f1dfb1f2016582c4393598235a
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-10 15:50:05 +0800

    add Chips

--- 03-basic-widgets/projects/starter/lib/card1.dart
+++ 03-basic-widgets/projects/starter/lib/card1.dart
@@ -17,6 +17,17 @@ class Card1 extends StatelessWidget {
     // 3
     return Center(
       child: Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/mag1.png'),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
         child: Stack(
           children: [
             Text(
@@ -48,17 +59,6 @@ class Card1 extends StatelessWidget {
             ),
           ],
         ),
-        padding: const EdgeInsets.all(16),
-        constraints: const BoxConstraints.expand(
-          width: 350,
-          height: 450,
-        ),
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/mag1.png'),
-              fit: BoxFit.cover,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(10.0))),
       ),
     );
   }
