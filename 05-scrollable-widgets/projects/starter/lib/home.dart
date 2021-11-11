a2dc69994e68f6b17ac187e9f986a6ce73181f97
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-11 14:50:53 +0800

    add recipes screen

--- 05-scrollable-widgets/projects/starter/lib/home.dart
+++ 05-scrollable-widgets/projects/starter/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/screens/recipes_screen.dart';
 import '/screens/explore_screen.dart';
 
 class Home extends StatefulWidget {
@@ -13,8 +14,7 @@ class _HomeState extends State<Home> {
 
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
-    // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
+    RecipesScreen(),
     Container(color: Colors.blue),
   ];
 
