7241722f040170e9fdbc6d246ddcc31365779d9c
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-11 16:37:57 +0800

    implement Empty grocery

--- 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -6,11 +6,36 @@ class EmptyGroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(30.0),
+      padding: const EdgeInsets.all(30.0),
       child: Center(
-        child:  Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            Flexible(
+                child: AspectRatio(
+              aspectRatio: 1 / 1,
+              child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+            )),
+            const Text(
+              'No Groceries',
+              style: TextStyle(fontSize: 21.0),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down？',
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(
+                textColor: Colors.white,
+                child: const Text('Browse Recipes'),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                color: Colors.green,
+                onPressed: () {
+                  // TODO 8: Go to Recipes Tab
+                })
+          ],
         ),
       ),
     );
