04a6d43a4b95473726f2dbb48c4d3e61ee3d5614
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-12 15:03:13 +0800

    Presenting GroceryItemScreen

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '/models/grocery_manager.dart';
 import 'package:provider/provider.dart';
 import '/screens/empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -14,7 +15,27 @@ class GroceryScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          // TODO 11: Present GroceryItemScreen
+          // 1
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          // 2
+          Navigator.push(
+            context,
+            // 3
+            MaterialPageRoute(
+              // 4
+              builder: (context) => GroceryItemScreen(
+                // 5
+                onCreate: (item) {
+                  // 6
+                  manager.addItem(item);
+                  // 7
+                  Navigator.pop(context);
+                },
+                // 8
+                onUpdate: (item) {},
+              ),
+            ),
+          );
         },
       ),
       // 7
