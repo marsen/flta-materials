6e074753af35cc92aa8813b00e3aa633dede45cc
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-17 20:06:11 +0800

    add login / completeOnboarding / gotoTab / goToRecipes

--- 07-routes-and-navigation/projects/starter/lib/models/app_state_manager.dart
+++ 07-routes-and-navigation/projects/starter/lib/models/app_state_manager.dart
@@ -12,29 +12,62 @@ class FooderlichTab {
 class AppStateManager extends ChangeNotifier {
   // 2
   bool _initialized = false;
+
   // 3
   bool _loggedIn = false;
+
   // 4
   bool _onboardingComplete = false;
+
   // 5
   int _selectedTab = FooderlichTab.explore;
 
   // 6
   bool get isInitialized => _initialized;
+
   bool get isLoggedIn => _loggedIn;
+
   bool get isOnboardingComplete => _onboardingComplete;
+
   int get getSelectedTab => _selectedTab;
 
-void initializeApp(){
-  Timer(const Duration(microseconds: 2000),(){
-    _initialized = true;
-    notifyListeners();
-  });
-}
+  void initializeApp() {
+    Timer(const Duration(microseconds: 2000), () {
+      _initialized = true;
+      notifyListeners();
+    });
+  }
 
-// TODO: Add login
-// TODO: Add completeOnboarding
-// TODO: Add goToTab
-// TODO: Add goToRecipes
-// TODO: Add logout
+  void login(String username, String password) {
+    _loggedIn = true;
+    notifyListeners();
+  }
+
+  void completeOnboarding() {
+    _onboardingComplete = true;
+    notifyListeners();
+  }
+
+  void goToTab(index) {
+    _selectedTab = index;
+    notifyListeners();
+  }
+
+  void goToRecipes() {
+    _selectedTab = FooderlichTab.recipes;
+    notifyListeners();
+  }
+
+  void logout() {
+    // 12
+    _loggedIn = false;
+    _onboardingComplete = false;
+    _initialized = false;
+    _selectedTab = 0;
+
+    // 13
+    initializeApp();
+    // 14
+    notifyListeners();
+  }
 }
