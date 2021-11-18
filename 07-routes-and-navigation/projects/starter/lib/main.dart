bce354118af1f57d7984c60835c404b867bb27bf
Mark Lin <mark.lin@ttfb-lab.com>
2021-11-18 18:03:26 +0800

    import app router

--- 07-routes-and-navigation/projects/starter/lib/main.dart
+++ 07-routes-and-navigation/projects/starter/lib/main.dart
@@ -3,8 +3,9 @@ import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'screens/splash_screen.dart';
 // TODO: Import app_router
+import 'navigation/app_router.dart';
+
 
 void main() {
   runApp(
@@ -26,8 +27,19 @@ class _FooderlichState extends State<Fooderlich> {
   final _appStateManager = AppStateManager();
 
   // TODO: Define AppRouter
+  late AppRouter _appRouter;
 
   // TODO: Initialize app router
+  @override
+  void initState() {
+    _appRouter = AppRouter(
+      appStateManager: _appStateManager,
+      groceryManager: _groceryManager,
+      profileManager: _profileManager,
+    );
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +67,10 @@ class _FooderlichState extends State<Fooderlich> {
             theme: theme,
             title: 'Fooderlich',
             // TODO: Replace with Router widget
-            home: const SplashScreen(),
+            home: Router(
+              routerDelegate: _appRouter,
+              // TODO: Add backButtonDispatcher
+            ),
           );
         },
       ),
